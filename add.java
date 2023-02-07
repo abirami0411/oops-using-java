1275fe586441422fc9082fd8472d1fc916500c2d
abirami0411 <124653177+abirami0411@users.noreply.github.com>
2023-02-07 10:45:16 +0530

    sample

---
+++ add.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class add
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a");
+int a=sc.nextInt();
+System.out.println("enter b");
+int b=sc.nextInt();
+int c=a+b;
+System.out.println("C is");
+System.out.println(c);
+}
+}       
